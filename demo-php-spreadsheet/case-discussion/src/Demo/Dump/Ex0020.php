783065c495600ab8c67945e154fff358944eb8c4
samwhelp <samwhelp@users.noreply.github.com>
2019-02-27 00:05:31 +0800

    remove comment

--- demo-php-spreadsheet/case-discussion/src/Demo/Dump/Ex0020.php
+++ demo-php-spreadsheet/case-discussion/src/Demo/Dump/Ex0020.php
@@ -40,25 +40,6 @@ class Ex0020 {
 		echo PHP_EOL;
 	}
 
-	/*
-	protected function dump_1 ()
-	{
-		$file_path = THE_ASSET_DIR_PATH . '/merge/1.ods';
-
-		echo 'Dump file: ' . $file_path;
-		echo PHP_EOL;
-		echo PHP_EOL;
-
-		$spreadsheet = IOFactory::load($file_path);
-
-		$data = $spreadsheet->getActiveSheet()->toArray(null, true, true, true);
-
-		var_dump($data);
-
-		echo PHP_EOL;
-	}
-	*/
-
 	protected function dump_1 ()
 	{
 		$this->dump_file(THE_ASSET_DIR_PATH . '/merge/1.ods');
