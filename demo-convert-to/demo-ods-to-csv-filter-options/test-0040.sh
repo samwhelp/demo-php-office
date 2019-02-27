fb769913558c509af2087ffba065e1b0a71a50ba
samwhelp <samwhelp@users.noreply.github.com>
2019-02-27 12:27:10 +0800

    add link

--- demo-convert-to/demo-ods-to-csv-filter-options/test-0040.sh
+++ demo-convert-to/demo-ods-to-csv-filter-options/test-0040.sh
@@ -6,3 +6,4 @@ localc --headless --convert-to csv:"Text - txt - csv (StarCalc)":44,34,UTF8 demo
 # https://wiki.openoffice.org/wiki/Documentation/DevGuide/Spreadsheets/Filter_Options#Tokens_1_to_5
 # https://wiki.openoffice.org/wiki/Documentation/DevGuide/Spreadsheets/Filter_Options#Examples
 # https://wiki.openoffice.org/wiki/Documentation/DevGuide/Spreadsheets/Filter_Options#Filter_Options_for_Lotus.2C_dBase_and_DIF_Filters
+# https://github.com/dagwieers/unoconv/blob/master/unoconv#L1090
